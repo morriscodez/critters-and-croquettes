ebca5349f817c11f73cbc653d8a90081ae918e17
Dylan Morris <dylanrobertmorris@gmail.com>
2021-04-22 10:00:01 -0500

    finished ch2

---
+++ animals.py
@@ -0,0 +1,20 @@
+
+from exercises.petting_zoo import Cobra, Copperhead, Donkey, Eel, Garden_Snake, Goat, King_Snake, Llama, Mallard, Milk_Frog, Rat_Snake, Rhinoceros, Sloth, Slow_Loris, Tortoise
+# from .cobra import Cobra
+# from .copperhead import Copperhead 
+
+miss_fuzz = Llama()
+mr_madof = Rat_Snake()
+mr_charm = Cobra()
+miss_goldy = Copperhead()
+mr_strong = Donkey()
+ms_buzz = Eel()
+mr_flower = Garden_Snake()
+mr_curly = Goat()
+mr_tiny = King_Snake()
+dr_quack = Mallard()
+miss_misty = Milk_Frog()
+mr_gentle = Rhinoceros()
+miss_chill = Sloth()
+mr_careful = Slow_Loris()
+miss_wise = Tortoise()
